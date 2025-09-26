f15e045ebcf4ca110c24902140611b713f9cd772
Nguyen Duong Ninh <ndninh.040303@gmail.com>
2025-09-26 21:42:38 +0700

    fix(project): fix some bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   const config = new DocumentBuilder()
     .setTitle('Products API')
     .setDescription('API demo Search/Filter/Pagination')
@@ -11,6 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
